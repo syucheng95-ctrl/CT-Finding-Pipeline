1ea37a8070c5134ad081a53b495c13b0612664fc
syucheng <syucheng95@gmail.com>
2026-05-14 20:06:00 +0800

    Allow vendored VoxTell without package metadata

--- workspace/voxtell/voxtell/__init__.py
+++ workspace/voxtell/voxtell/__init__.py
@@ -1,3 +1,6 @@
-from importlib.metadata import version as _version
+from importlib.metadata import PackageNotFoundError, version as _version
 
-__version__ = _version("voxtell")
+try:
+    __version__ = _version("voxtell")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
